24300e24e5601755e72fd29194eafff88b9c7b1e
Amir Abouei <abouei.me@gmail.com>
2022-07-01 09:54:35 +0430

    feat: Complete Season Controller (index, view)

--- app/Http/Controllers/SeasonController.php
+++ app/Http/Controllers/SeasonController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\WhiteHouse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 class SeasonController extends Controller
@@ -10,13 +13,34 @@ class SeasonController extends Controller
 
     public function index(Request $request)
     {
+        try {
+            $data = DB::select("SELECT seasons.number, seasons.rating, seasons.ranking
+                                   FROM seasons
+                                   WHERE seasons.account_id = {$request->user_id}");
 
+            return WhiteHouse::generalResponse(Response::HTTP_OK, $data);
+        } catch (Exception $ex) {
+            return WhiteHouse::generalResponse(Response::HTTP_INTERNAL_SERVER_ERROR, WhiteHouse::SERVER_ERROR_MESSAGE);
+        }
     }
 
 
     public function show(Request $request)
     {
-       return $request->season_num;
+        try {
+            $data = DB::select("SELECT seasons.number, seasons.rating, seasons.ranking
+                                   FROM seasons
+                                   WHERE seasons.account_id = {$request->user_id}
+                                   AND seasons.number = {$request->season_num}");
+
+            if (empty($data)){
+                return WhiteHouse::generalResponse(Response::HTTP_UNPROCESSABLE_ENTITY, 'Invalid Season number');
+            }
+
+            return WhiteHouse::generalResponse(Response::HTTP_OK, $data);
+        } catch (Exception $ex) {
+            return WhiteHouse::generalResponse(Response::HTTP_INTERNAL_SERVER_ERROR, WhiteHouse::SERVER_ERROR_MESSAGE);
+        }
     }
 
 }
