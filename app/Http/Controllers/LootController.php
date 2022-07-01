985ace6facbb8a3513f0403c46988f599352c804
Amir Abouei <abouei.me@gmail.com>
2022-07-01 09:32:33 +0430

    feat: Complete loot controller (index and store)

--- app/Http/Controllers/LootController.php
+++ app/Http/Controllers/LootController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\WhiteHouse;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
 
 class LootController extends Controller
 {
@@ -18,23 +22,47 @@ class LootController extends Controller
      */
     public function index(Request $request): JsonResponse
     {
-        $userId = (int)$request->user_id;
+        try {
+            $userId = (int)$request->user_id;
 
-        $rawQuery = DB::raw("SELECT loots.name, loots.mythic_essence_price, loots.game_credit_price, loots.type, loot_ownerships.count
+            $data = DB::select("SELECT loots.name, loot_ownerships.count
                                    FROM loots
                                    JOIN loot_ownerships
                                    ON loots.name = loot_ownerships.loot_name
                                    WHERE loot_ownerships.account_id = {$userId}");
-        $data     = DB::select($rawQuery);
 
-        return response()->json(['data' => $data]);
+            return WhiteHouse::generalResponse(Response::HTTP_OK, $data);
+        } catch (Exception $ex) {
+            return WhiteHouse::generalResponse(Response::HTTP_INTERNAL_SERVER_ERROR, WhiteHouse::SERVER_ERROR_MESSAGE);
+        }
     }
 
 
 
     public function store(Request $request)
     {
-        //DB::raw('SELECT count(*) FROM dbp')
+        try {
+            $userId           = $request->user_id;
+            $lootName         = Route::current()->parameter('loot_name');
+            $isLootNameExists = DB::selectOne("SELECT * FROM loots WHERE loots.name = '{$lootName}' ");
+            if (is_null($isLootNameExists)) {
+                return WhiteHouse::generalResponse(Response::HTTP_UNPROCESSABLE_ENTITY, 'Invalid Loot Name');
+            }
+
+            $currentLootNameExisting = DB::selectOne("SELECT * FROM loot_ownerships
+                                                         WHERE loot_ownerships.account_id = {$userId}
+                                                         AND loot_ownerships.loot_name = '{$lootName}' ");
+
+            if (is_null($currentLootNameExisting)) {
+                DB::insert("INSERT INTO loot_ownerships (account_id, loot_name, count) VALUES ($userId, '$lootName', 1)");
+            } else {
+                DB::update("UPDATE loot_ownerships SET count = ({$currentLootNameExisting->count} + 1) WHERE account_id = {$userId} AND loot_name = '{$lootName}' ");
+            }
+
+            return $this->index($request);
+        } catch (Exception $ex) {
+            return WhiteHouse::generalResponse(Response::HTTP_INTERNAL_SERVER_ERROR, WhiteHouse::SERVER_ERROR_MESSAGE);
+        }
     }
 
 }
