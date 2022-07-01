7c19dc6999764063d436a7cceaf1eb50592b6e3e
Amir Abouei <abouei.me@gmail.com>
2022-07-01 10:23:49 +0430

    feat: Use ModelViewableColumns trait for make specify viewable columns for SeasonController

--- app/Http/Controllers/SeasonController.php
+++ app/Http/Controllers/SeasonController.php
@@ -3,18 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\WhiteHouse;
+use App\Traits\ModelViewableColumns;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Route;
 
 class SeasonController extends Controller
 {
+    use ModelViewableColumns;
 
-    public function index(Request $request)
+    /**
+     * @var string[]
+     */
+    protected array $viewableColumns = [
+        'seasons.number',
+        'seasons.rating',
+        'seasons.ranking',
+    ];
+
+
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
     {
         try {
-            $data = DB::select("SELECT seasons.number, seasons.rating, seasons.ranking
+            $data = DB::select("SELECT {$this->getViewableColumns()}
                                    FROM seasons
                                    WHERE seasons.account_id = {$request->user_id}");
 
@@ -25,16 +43,22 @@ class SeasonController extends Controller
     }
 
 
-    public function show(Request $request)
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function show(Request $request): JsonResponse
     {
         try {
-            $data = DB::select("SELECT seasons.number, seasons.rating, seasons.ranking
+            $data = DB::select("SELECT {$this->getViewableColumns()}
                                    FROM seasons
                                    WHERE seasons.account_id = {$request->user_id}
                                    AND seasons.number = {$request->season_num}");
 
-            if (empty($data)){
-                return WhiteHouse::generalResponse(Response::HTTP_UNPROCESSABLE_ENTITY, 'Invalid Season number');
+            if (empty($data)) {
+                return WhiteHouse::generalResponse(Response::HTTP_UNPROCESSABLE_ENTITY, 'Invalid Season Number');
             }
 
             return WhiteHouse::generalResponse(Response::HTTP_OK, $data);
@@ -42,5 +66,4 @@ class SeasonController extends Controller
             return WhiteHouse::generalResponse(Response::HTTP_INTERNAL_SERVER_ERROR, WhiteHouse::SERVER_ERROR_MESSAGE);
         }
     }
-
 }
